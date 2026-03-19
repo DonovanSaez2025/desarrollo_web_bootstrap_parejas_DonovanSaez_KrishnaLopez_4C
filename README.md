80358445189098d76d981b1362260056b73fb525
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-03-19 10:06:22 -0300

    g
    g

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# desarrollo_web_bootstrap_parejas_DonovanSaez_KrishnaLopez_4C
-Evaluación de Desarrollo Web
+# Templo de los juegos
+**Autor**: Donovan Sáez R.
+
+**Contacto**: donovansaez@liceovvh.cl
+
+## Descripción
+Un sitio web para jugar y calificar millones de videojuegos creados por los propios usuarios y empresas de manera gratis y directa.
--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,44 @@
-console.log("Conectado a JS...");
-let username = localStorage.getItem("username") || "";
-const usernameSpan = document.getElementById("username");
-const botonSesion = document.getElementById("boton-sesion");
+console.log("Conectado a JS..."); // Imprime mensaje en la consola
+let username = localStorage.getItem("username") || ""; // Crea una variable y se le asigna un valor del localStorage, y si no existe, el valor default es vacío
+const usernameSpan = document.getElementById("username"); // Obtiene el span donde se mostrará el nombre de usuario
+const botonSesion = document.getElementById("boton-sesion"); // Obtiene el botón para inciciar sesión
 
+// Añade al escuchador de eventos cuando el documento HTML haya cargado
 document.addEventListener('DOMContentLoaded', () => {
-    if (username == "") {
-        iniciarSesion();
+    if (username == "") { // Si el username es vacío
+        iniciarSesion(); // Llama a la función iniciarSesion()
+    } else {
+        usernameSpan.textContent = username; // Establece el contenido del span con el nombre de usuario
     };
-    usernameSpan.textContent = username;
 });
 
+// Crea la función cerrar sesión
 function cerrarSesion() {
-    let confirmacion = confirm("¿Seguro que quieres cerrar sesión?");
+    let confirmacion = confirm("¿Seguro que quieres cerrar sesión?"); // Crea una variable y se le asigna de valor una confirmación, en la que aceptar es true y cancelar es false
 
-    if (confirmacion == true) {
-        localStorage.setItem("username", "");
-        username = "";
-        usernameSpan.textContent = "";
-        botonSesion.classList.remove("btn-danger");
-        botonSesion.classList.add("btn-success");
-        botonSesion.textContent = "Iniciar sesión";
-        botonSesion.onclick = iniciarSesion;
+    if (confirmacion == true) { // si confirmacion es verdadero
+        localStorage.setItem("username", ""); // Vacía el nombre de usuario del localStorage
+        username = ""; // Asigna el nombre de usuario a vacío
+        usernameSpan.textContent = ""; // Elimina el texto del span
+        botonSesion.classList.remove("btn-danger"); // Quita la clase btn-danger del botón de sesión
+        botonSesion.classList.add("btn-success"); // Añade la clase btn-success del botón de sesión
+        botonSesion.textContent = "Iniciar sesión"; // Cambia el texto del botón a "Iniciar sesión"
+        botonSesion.onclick = iniciarSesion; // Cambia la función dentro del "onclick" a iniciarSesion
     };
 };
 
+// Crea la función iniciarSesion
 function iniciarSesion() {
-    let usernameTempVer = prompt("Ingresa tu nombre de usuario");
-    usernameTempVer = usernameTempVer.trim();
-    if (usernameTempVer == "") {
-        alert("El nombre de usuario no puede estar vacío");
-        location.reload();
-    } else {
-        username = usernameTempVer;
-        localStorage.setItem("username", usernameTempVer)
-        botonSesion.classList.remove("btn-success");
-        botonSesion.classList.add("btn-danger");
-        botonSesion.textContent = "Cerrar  sesión";
-        botonSesion.onclick = cerrarSesion;
+    let usernameTempVer = prompt("Ingresa tu nombre de usuario"); // Crea una variable de verificacion temporal cuyo valor es ingresado por el usuario
+    usernameTempVer = usernameTempVer.trim(); // Elimina los espacios del inicio y el final del string
+    if (usernameTempVer == "") { // Si el nombre está vacío
+        alert("El nombre de usuario no puede estar vacío"); // Alerta diciendo que no puede estar vacío
+    } else { // o si no
+        username = usernameTempVer; // Establece la variable del nombre de usuario el valor de la verificacion temporal
+        localStorage.setItem("username", usernameTempVer) // Establece el dato del localStorage al valor de la verificacion temporal
+        botonSesion.classList.remove("btn-success"); // Quita la clase btn-success del botón de sesión
+        botonSesion.classList.add("btn-danger"); // Añade la clase btn-danger del botón de sesión
+        botonSesion.textContent = "Cerrar  sesión"; // Cambia el texto del botón a "Cerrar sesión"
+        usernameSpan.textContent = username; // Establece el contenido del span con el nombre de usuario
     };
-    usernameSpan.textContent = username;
 };
