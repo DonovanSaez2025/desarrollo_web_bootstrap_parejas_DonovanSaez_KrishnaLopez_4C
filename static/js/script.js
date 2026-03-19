6a51be71074b7144cb9f3414842c57aba765add0
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-03-19 09:01:16 -0300

    h
    h

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,39 @@
 console.log("Conectado a JS...");
 let username = localStorage.getItem("username") || "";
 const usernameSpan = document.getElementById("username");
+const botonSesion = document.getElementById("boton-sesion");
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (username == "") {
-        let usernameTempVer = prompt("Ingresa tu nombre de usuario");
-        usernameTempVer = usernameTempVer.trim();
-        if (usernameTempVer == "") {
-            alert("El nombre de usuario no puede estar vacío");
-            window.location.href = "/index.html";
-        } else {
-            username = usernameTempVer;
-            localStorage.setItem("username", usernameTempVer)
-        };
-    };
     usernameSpan.textContent = username;
 });
 
-function cerrarSesionConfirmacion(confirmacion) {
-    return confirm("¿Seguro que quieres cerrar sesión?");
+function cerrarSesion() {
+    let confirmacion = confirm("¿Seguro que quieres cerrar sesión?");
+
+    if (confirmacion == true) {
+        localStorage.setItem("username", "");
+        username = "";
+        usernameSpan.textContent = "";
+        botonSesion.classList.remove("btn-danger");
+        botonSesion.classList.add("btn-success");
+        botonSesion.textContent = "Iniciar sesión";
+        botonSesion.onclick = iniciarSesion;
+    };
 };
 
-function cerrarSesion() {
-    let confirmacion = cerrarSesionConfirmacion(confirmacion) {
-        
-    }
-}
+function iniciarSesion() {
+    let usernameTempVer = prompt("Ingresa tu nombre de usuario");
+    usernameTempVer = usernameTempVer.trim();
+    if (usernameTempVer == "") {
+        alert("El nombre de usuario no puede estar vacío");
+        location.reload();
+    } else {
+        username = usernameTempVer;
+        localStorage.setItem("username", usernameTempVer)
+        botonSesion.classList.remove("btn-success");
+        botonSesion.classList.add("btn-danger");
+        botonSesion.textContent = "Cerrar  sesión";
+        botonSesion.onclick = cerrarSesion;
+    };
+    usernameSpan.textContent = username;
+};
